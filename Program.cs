94d7c568c9b5cf966b58c01d614ac30e71d2618f
ivan.vorobiev <ivan.vorobiev@accenture.com>
2020-01-20 14:42:24 +0300

    Разработал геокодер

--- Program.cs
+++ Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
+using System.Globalization;
 
-namespace GeoRequest
+namespace MapRequest
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string url = "https://geocode-maps.yandex.ru/1.x/?apikey=";
+            string apiKey = "";
+
+            Console.WriteLine("Введите город: ");
+            string city = Console.ReadLine();
+            Console.WriteLine("Введите улицу: ");
+            string street = Console.ReadLine();
+            Console.WriteLine("Введите дом: ");
+            string house = Console.ReadLine();
+            string reqGeo = "&geocode=" + city + ",+" + street + "+улица,+дом" + house;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + apiKey + reqGeo);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream receiveStream = response.GetResponseStream();
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(readStream.ReadToEnd());
+            response.Close();
+            readStream.Close();
+
+            XmlNodeList elemList = doc.GetElementsByTagName("pos");
+
+            int indexSpace = 0;
+            for (int i = 0; i < elemList.Item(0).InnerText.Length; i++)
+            {
+                if (elemList.Item(0).InnerText[i] == ' ')
+                    indexSpace = i;
+            }
+            //Долгота
+            double longitude = double.Parse(elemList.Item(0).InnerText.Substring(0, indexSpace), CultureInfo.InvariantCulture);
+            //Широта
+            double latitude = double.Parse(elemList.Item(0).InnerText.Substring(indexSpace), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Координаты:\n" + latitude + " " + longitude);
+
+            Console.ReadLine();
         }
     }
 }
